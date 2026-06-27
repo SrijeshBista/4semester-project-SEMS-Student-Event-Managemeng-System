2db251e9ae491ce19f29f74ec7caa472c0066765
Srijesh Bista <srijeshbista367@gmail.com>
2026-06-27 18:16:01 +0545

    Just Finsihed all the ui now left login and dashbord

--- JS/Style.js
+++ JS/Style.js
@@ -11,3 +11,48 @@ closeMenu.addEventListener("click", () => {
     offscreenMenu.classList.remove("active");
     hamMenu.classList.remove("active");
 });
+
+
+// search 
+
+const searchInput = document.getElementById("searchInput");
+const categoryFilter = document.getElementById("categoryFilter");
+const searchBtn = document.getElementById("searchBtn");
+
+const eventCards = document.querySelectorAll(".event-card");
+
+function filterEvents(){
+
+    const searchValue = searchInput.value.toLowerCase();
+    const selectedCategory = categoryFilter.value;
+
+    eventCards.forEach(card=>{
+
+        const title = card.querySelector("h3").textContent.toLowerCase();
+        const category = card.dataset.category;
+
+        const matchTitle = title.includes(searchValue);
+
+        const matchCategory =
+        selectedCategory === "all" ||
+        category === selectedCategory;
+
+        if(matchTitle && matchCategory){
+
+            card.style.display="block";
+
+        }else{
+
+            card.style.display="none";
+
+        }
+
+    });
+
+}
+
+searchBtn.addEventListener("click",filterEvents);
+
+searchInput.addEventListener("keyup",filterEvents);
+
+categoryFilter.addEventListener("change",filterEvents);
